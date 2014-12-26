ac4444509511eb95c70e770dec96ffa43e612fbf
Olga Grabek <olga.grabek@gmail.com>
2014-12-26 18:05:09 +0100

    add HTTP parser

--- lib/tamarin/connection.rb
+++ lib/tamarin/connection.rb
@@ -1,19 +1,32 @@
+require 'http/parser'
+
 module Tamarin
 	
 	class Connection
 
-		attr_accessor :socket
+		attr_accessor :socket, :parser
 		
 		def initialize(socket)
 			@socket = socket
+			@parser = Http::Parser.new(self)
 		end
 
 		def process
-			data = socket.readpartial(1024)
-			puts data
+			until socket.closed? || socket.eof?
+				data = socket.readpartial(1024)
+				parser << data
+			end
+		end
+
+		def on_message_complete
+			puts "parser #{parser.headers}" 
+			puts "parser #{parser.http_method}" 
+			puts "parser #{parser.request_url}" 
 			write_response
 			close			
 		end
+
+		private
 
 		def write_response
 			socket.write "HTTP/1.1 200 OK\r\n"
